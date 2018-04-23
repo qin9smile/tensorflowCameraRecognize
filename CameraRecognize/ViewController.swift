1a8575f3c75ea31b995a9a3fb5289072add75264
charlotte <hotstrip@yahoo.com>
2018-04-23 10:41:18 +0800

    Initial Commit

---
+++ CameraRecognize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraRecognize
+//
+//  Created by 宇宙超级无敌可爱美少女 on 2018/4/23.
+//  Copyright © 2018年 宇宙超级无敌可爱美少女. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
