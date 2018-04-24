5011c66eea74de095b6b8c4fa13bfbb2cd6c1ef6
charlotte <hotstrip@yahoo.com>
2018-04-24 11:37:20 +0800

    add camera view

--- CameraRecognize/ViewController.swift
+++ CameraRecognize/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+let MODEL_FILE_NAME: String = "tensorflow_inception_graph"
+let LABEL_FILE_NAME: String = "imagenet_comp_graph_label_strings"
 
 class ViewController: UIViewController {
-
+  var predicateLabel: UILabel!
+  var cameraView: CameraView!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    cameraView = CameraView(frame: view.frame)
+    cameraView.delegate = self
+//    cameraView.overlayPath = createBezierPath()
+    cameraView.scanAnimationDirection = .horizontal
+    view.addSubview(cameraView)
+    
+    predicateLabel = UILabel(frame: CGRect(x: 20, y: 70, width: 300, height: 30))
+    predicateLabel.textColor = .white
+    predicateLabel.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+    predicateLabel.text = "识别率："
+    view.addSubview(predicateLabel)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
+extension ViewController: CameraViewDelegate {
+  func cameraView(_ cameraView: CameraView, predictionValue value: CGFloat, predictionImage image: UIImage) {
+    predicateLabel.text = String(format: "识别率: %.4f", value)
+  }
+}
