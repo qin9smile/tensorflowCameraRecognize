c82d74db1673d8914b9115db439d0ef3db933b33
charlotte <hotstrip@yahoo.com>
2018-04-26 13:29:48 +0800

    add camera recognition framework

---
+++ CameraRecognition/CameraRecognition.h
@@ -0,0 +1,19 @@
+//
+//  CameraRecognition.h
+//  CameraRecognition
+//
+//  Created by 宇宙超级无敌可爱美少女 on 2018/4/26.
+//  Copyright © 2018年 宇宙超级无敌可爱美少女. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraRecognition.
+FOUNDATION_EXPORT double CameraRecognitionVersionNumber;
+
+//! Project version string for CameraRecognition.
+FOUNDATION_EXPORT const unsigned char CameraRecognitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraRecognition/PublicHeader.h>
+
+
